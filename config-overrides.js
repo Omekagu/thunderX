5ced9c66055af3a41450a5d7d212782090dec178
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-07-01 09:58:21 +0100

    chore: update dependencies and configure webpack for browser compatibility
    - Updated various dependencies in package.json including ajv, ethers, jspdf, and others.
    - Changed react-scripts to react-app-rewired for custom configuration.
    - Added config-overrides.js to provide fallbacks for Node.js core modules in the browser environment.

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+
+module.exports = function override (config) {
+  config.resolve = config.resolve || {}
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert/'),
+    util: require.resolve('util/'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser.js'),
+    url: require.resolve('url/'),
+    zlib: require.resolve('browserify-zlib'),
+    buffer: require.resolve('buffer/'),
+    process: require.resolve('process/browser.js')
+  }
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser.js'
+    })
+  ])
+  return config
+}
